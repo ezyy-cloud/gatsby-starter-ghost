6adda640b2d95cd7a311f97f684440e9547030a8
Mafikizolo <62421697+KloudCoder@users.noreply.github.com>
2020-12-06 23:39:46 +0200

    Removed post title and featured image
    Post should be body only. This is the creator's canvas

--- src/templates/post.js
+++ src/templates/post.js
@@ -28,12 +28,7 @@ const Post = ({ data, location }) => {
             <Layout>
                 <div className="container">
                     <article className="content">
-                        { post.feature_image ?
-                            <figure className="post-feature-image">
-                                <img src={ post.feature_image } alt={ post.title } />
-                            </figure> : null }
                         <section className="post-full-content">
-                            <h1 className="content-title">{post.title}</h1>
 
                             {/* The main post content */ }
                             <section
